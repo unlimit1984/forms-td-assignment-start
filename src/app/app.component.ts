aa6052054ea4f828c61620e11671182fcb1463e3
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-11 21:21:56 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f', {static: true}) signupForm: NgForm;
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
