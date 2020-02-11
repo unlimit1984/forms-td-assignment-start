0b160c598890469feadf99641f33d44bc631f278
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-11 21:24:27 +0100

    Forms TD [199] make assessment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   @ViewChild('f', {static: true}) signupForm: NgForm;
 
   onSubmit() {
